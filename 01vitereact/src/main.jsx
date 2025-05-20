b662f908b524e27489de28957fe91f54180e66b5
jadhavsiid <siidjadhav170801@gmail.com>
2025-05-20 22:40:31 +0530

    Second day-partial commit 22:40 20-05-2025

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!!</h1>
+    </div>
+  );
+}
+
+// Correctly defined React element using createElement
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://www.google.com', target: '_blank' },
+  'Click me to visit Google'
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
-)
+    <MyApp />
+    {reactElement}
+  </>
+);
